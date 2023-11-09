e7e06423d5aab0cb55c741ea3127d86183619fb6
LiranBratt2121 <brattlirannin@gmail.com>
2023-11-09 20:44:35 +0200

    Arm: Simplify arm logic

--- src/main/java/frc/robot/subsystems/Arm.java
+++ src/main/java/frc/robot/subsystems/Arm.java
@@ -11,43 +11,7 @@ public class Arm extends SubsystemBase {
     m_armMotor = new Spark(ArmConstants.kArmMotor);
   }
 
-  public enum ArmState {
-    HUMAN_PLAYER,
-    TOP,
-    MID,
-    BOTTOM,
-    RESET
-  }
-
-  private void set(double speed) {
+  public void set(double speed) {
     m_armMotor.set(speed);
-  }
-
-  public void toggleState(ArmState state) {
-    switch (state) {
-      case HUMAN_PLAYER:
-        this.set(ArmConstants.kHumanPlayerSpeed);
-        break;
-
-      case TOP:
-        this.set(ArmConstants.kTopSpeed);
-        break;
-
-      case MID:
-        this.set(ArmConstants.kMidSpeed);
-        break;
-
-      case BOTTOM:
-        this.set(ArmConstants.kBottomSpeed);
-        break;
-
-      case RESET:
-        this.set(ArmConstants.kResetToDefault);
-        break;
-
-      default:
-        this.set(0);
-        break;
-    }
   }
 }
