ead419f275f4d66388bd7c8c23571c69e533a557
LiranBratt2121 <brattlirannin@gmail.com>
2023-11-09 18:01:34 +0200

    Intake: Create Intake subsystem

---
+++ src/main/java/frc/robot/subsystem/Intake.java
@@ -0,0 +1,11 @@
+package frc.robot.subsystem;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Intake extends SubsystemBase {
+  public Intake() {}
+
+  @Override
+  public void periodic() {
+  }
+}
