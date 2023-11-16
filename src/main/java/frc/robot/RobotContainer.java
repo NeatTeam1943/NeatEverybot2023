e921a4e6148072682d2b1e2a184e80c4677b3074
LiranBratt2121 <brattlirannin@gmail.com>
2023-11-16 18:52:58 +0200

    RobotContainer: Add Sequents to code

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -5,9 +5,17 @@
 package frc.robot;
 
 import frc.robot.Constants.OperatorConstants;
+import frc.robot.commands.ArmHandler;
+import frc.robot.commands.IntakeHandler;
+import frc.robot.subsystems.Arm;
 import frc.robot.subsystems.Chassis;
+import frc.robot.subsystems.Intake;
+import frc.robot.subsystems.Arm.ArmState;
+import frc.robot.subsystems.Intake.IntakeState;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.RunCommand;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 
 public class RobotContainer {
@@ -15,9 +23,13 @@ public class RobotContainer {
       new CommandXboxController(OperatorConstants.kDriverControllerPort);
 
   private final Chassis m_chassis;
+  private final Arm m_arm;
+  private final Intake m_intake;
 
   public RobotContainer() {
     m_chassis = new Chassis();
+    m_arm = new Arm();
+    m_intake = new Intake();
 
     m_chassis.setDefaultCommand(new RunCommand(() -> m_chassis.move(m_driverController), m_chassis));
     
@@ -25,6 +37,14 @@ public class RobotContainer {
   }
 
   private void configureBindings() {
+    m_driverController.a().whileTrue(new SequentialCommandGroup(
+      new ArmHandler(m_arm, ArmState.TOP),
+      new IntakeHandler(m_intake, IntakeState.EJECT),
+      new WaitCommand(3),
+      new IntakeHandler(m_intake, IntakeState.RESET),
+      new WaitCommand(3),
+      new ArmHandler(m_arm, ArmState.RESET)
+    ));
   }
 
   public Command getAutonomousCommand() {
