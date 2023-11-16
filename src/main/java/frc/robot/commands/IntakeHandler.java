7c78463c7b99dde6ea4114034e33516d3a4b4f96
LiranBratt2121 <brattlirannin@gmail.com>
2023-11-16 18:09:33 +0200

    Intake: Add Intake State handler command

---
+++ src/main/java/frc/robot/commands/IntakeHandler.java
@@ -0,0 +1,22 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj.motorcontrol.Spark;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystem.Intake;
+import frc.robot.subsystem.Intake.IntakeState;
+
+public class IntakeHandler extends CommandBase {
+  private Intake m_intake; 
+  private int m_speed;
+
+  public IntakeHandler(Intake intake, IntakeState state) {
+    m_intake = intake;
+
+    m_speed = state.getSpeed();
+  }
+
+  @Override
+  public void initialize() {
+    m_intake.set(m_speed);
+  }
+}
